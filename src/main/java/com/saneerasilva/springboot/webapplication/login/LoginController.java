098372dab4796438a899ffeba0d9e181749ff0a8
Saneera Silva <saneera@eight25meida.com>
2024-05-05 00:32:13 +0800

    login initial commit

---
+++ src/main/java/com/saneerasilva/springboot/webapplication/login/LoginController.java
@@ -0,0 +1,17 @@
+package com.saneerasilva.springboot.webapplication.login;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class LoginController {
+	
+	///login => com.in28minutes.springboot.myfirstwebapp.login.LoginController => login.jsp
+	
+	@RequestMapping("login")
+	public String gotoLoginPage() {
+		return "login";
+	}
+}
