5c6f182f01cf661055906c5630415731bb365172
Saneera Silva <saneera@eight25meida.com>
2024-05-05 19:40:09 +0800

    add todo controller and service

---
+++ src/main/java/com/saneerasilva/springboot/webapplication/todo/TodoService.java
@@ -0,0 +1,24 @@
+package com.saneerasilva.springboot.webapplication.todo;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+	private static List<Todo> todos = new ArrayList<>();
+	
+	static {
+		todos.add(new Todo(1,"Saneera", "Learn AWS", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(1,"Saneera", "Learn Devops", LocalDate.now().plusYears(1), false));
+		todos.add(new Todo(1,"Saneera", "Learn Fullstack", LocalDate.now().plusYears(1), false));
+
+	}
+	
+	public List<Todo> findByUsername(String username){
+		return todos;
+	}
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Todos List</title>
+</head>
+<body>
+	<div>Welcome</div>
+	<div>Todos ${todos}</div>
+</body>
+</html>
